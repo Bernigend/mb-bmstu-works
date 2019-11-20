76897b361900f1c9aa2f711c79d7ea1bff14827d
Andrey Piksaev <pikcaev@gmail.com>
2019-11-20 21:48:38 +0300

    Добавлены требуемые по условию команды

---
+++ 1-year/programming-in-high-level-languages/laboratory-work-1/main.cpp
@@ -0,0 +1,89 @@
+/**
+ * Created by Bernigend on 20.11.2019.
+ *
+ * ---
+ *
+ * Лабораторная работа №1
+ * Написать консольный скрипт, на вход которому будут даваться команды и аргументы к ним
+ * Требуемые команды:
+ * - 1. Вывод доступных команд
+ * - 2. Вывод количества байт, отводимых под каждый тип переменной
+ * - 3. Перевод числа в систему счисления с основанием 16
+ */
+
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main (int argc, char *argv[])
+{
+	if (argc < 2) {
+		cout << endl;
+		cout << "Laboratory work No. 1" << endl << endl;
+		cout << "Usage:" << endl;
+		cout << "command [argument]" << endl << endl;
+		cout << "To view a list of commands, use --help";
+		cout << endl;
+
+		return EXIT_SUCCESS;
+	}
+
+	string userCommand = argv[1];
+
+	cout << endl;
+
+	if (userCommand == "--help") {
+
+		cout << "Laboratory work No. 1" << endl << endl;
+		cout << "Usage:" << endl;
+		cout << setw(14) << left << "--help"         << " - displays a list of commands" << endl;
+		cout << setw(14) << left << "--sizes"        << " - displays variable sizes" << endl;
+		cout << setw(14) << left << "--hex [number]" << " - displays the number converted from 10 number system to 16 number system" << endl;
+
+	} else if (userCommand == "--sizes") {
+
+		short width = 18;
+
+		cout << "Variable sizes:" << endl;
+		cout << endl;
+
+		cout << setw(width) << left << "short" << " - " << sizeof(short) << " bytes" << endl;
+		cout << setw(width) << left << "unsigned short" << " - " << sizeof(unsigned short) << " bytes" << endl;
+		cout << setw(width) << left << "short int" << " - " << sizeof(short int) << " bytes" << endl;
+		cout << setw(width) << left << "unsigned short int" << " - " << sizeof(unsigned short int) << " bytes" << endl;
+		cout << setw(width) << left << "int" << " - " << sizeof(int) << " bytes" << endl;
+		cout << setw(width) << left << "unsigned int" << " - " << sizeof(unsigned int) << " bytes" << endl;
+		cout << setw(width) << left << "long" << " - " << sizeof(int) << " bytes" << endl;
+		cout << setw(width) << left << "unsigned long" << " - " << sizeof(int) << " bytes" << endl;
+		cout << setw(width) << left << "long long" << " - " << sizeof(int) << " bytes" << endl;
+		cout << setw(width) << left << "unsigned long long" << " - " << sizeof(int) << " bytes" << endl;
+		cout << endl;
+
+		cout << setw(width) << left << "float" << " - " << sizeof(float) << " bytes" << endl;
+		cout << setw(width) << left << "double" << " - " << sizeof(double) << " bytes" << endl;
+		cout << setw(width) << left << "long double" << " - " << sizeof(long double) << " bytes" << endl;
+		cout << endl;
+
+		cout << setw(width) << left << "char" << " - " << sizeof(char) << " bytes" << endl;
+		cout << setw(width) << left << "string" << " - " << sizeof(string) << " bytes" << endl;
+		cout << endl;
+
+		cout << setw(width) << left << "bool" << " - " << sizeof(bool) << " bytes";
+
+	} else if (userCommand == "--hex") {
+
+		if (argc < 3) {
+			cout << "You must enter a number in the 10 number system as an argument." << endl;
+			cout << "For example: --hex 12" << endl;
+
+			return EXIT_SUCCESS;
+		}
+
+		printf_s("Entered number (%s) in 16 number system: %X", argv[2], stoi(argv[2]));
+
+	}
+
+	cout << endl;
+	return EXIT_SUCCESS;
+}
