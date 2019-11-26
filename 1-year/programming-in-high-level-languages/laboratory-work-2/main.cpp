02f1a10daa836ae8993325aa6c6876981f10da54
Andrey Piksaev <pikcaev@gmail.com>
2019-11-26 10:06:14 +0300

    Добавлены пояснения

--- 1-year/programming-in-high-level-languages/laboratory-work-2/main.cpp
+++ 1-year/programming-in-high-level-languages/laboratory-work-2/main.cpp
@@ -22,10 +22,20 @@ int dataSegment = 123;
 int main(int argc, char* argv)
 {
 	int width = 14;
+
+	// Попадает в text segment, потому что является кодом программы
 	cout << setw(width) << left << "TEXT segment: " << main << endl;
+
+	// Попадает в bss segment, потому что переменная не инициализирована
 	cout << setw(width) << left << "BSS segment: " << &bssSegment << endl;
+	
+	// Попадает в data segment, потому что переменная инициализирована
 	cout << setw(width) << left << "DATA segment: " << &dataSegment << endl;
+	
+	// Попадает в heap, потому что память выделяется во время работы программы
 	cout << setw(width) << left << "HEAP: " << malloc(8) << endl;
+	
+	// Попадает в stack, потому что является локальный переменной функции
 	cout << setw(width) << left << "Stack: " << &width << endl;
 
 	return EXIT_SUCCESS;
