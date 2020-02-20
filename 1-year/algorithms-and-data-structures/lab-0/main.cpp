8fb50928da2da139b7213d7b78b773c57fec46df
Andrey Piksaev <pikcaev@gmail.com>
2020-02-20 22:59:28 +0300

    Create main.cpp

---
+++ 1-year/algorithms-and-data-structures/lab-0/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include "Set.h"
+
+int main()
+{
+    int numbers[3] = {1,2,3};
+    Set* set1 = new Set(3, numbers);
+    set1->show();
+    std::cout << std::endl;
+
+    int numbers2[3] = {2,2,6};
+    Set* set2 = new Set(3, numbers2);
+    set2->show();
+    std::cout << std::endl;
+
+//    set1->uniteWith(set2);
+//    set1->intersectionWith(set2);
+//    set1->difference(set2);
+
+    set1->show();
+}
