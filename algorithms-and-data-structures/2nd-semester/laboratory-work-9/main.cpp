28e579a21f09cb40c597ede07e05261cc895d9d0
Bernigend <pikcaev@gmail.com>
2020-06-06 01:06:55 +0300

    Изменена Лаб№9 по АСД

--- algorithms-and-data-structures/2nd-semester/laboratory-work-9/main.cpp
+++ algorithms-and-data-structures/2nd-semester/laboratory-work-9/main.cpp
@@ -7,69 +7,35 @@
 int main() {
     const unsigned int GRAPH_SIZE = 9;
 
+    // изображение указанного графа находится в папке images
     bool map[GRAPH_SIZE][GRAPH_SIZE] = {
-        { 0, 0, 1, 1, 0, 0, 0, 1, 1 },
-        { 0, 0, 0, 0, 0, 1, 1, 0, 0 },
-        { 1, 0, 0, 0, 1, 1, 0, 1, 0 },
-        { 1, 0, 0, 0, 0, 0, 0, 0, 1 },
-        { 0, 0, 1, 0, 0, 0, 0, 0, 0 },
-        { 0, 1, 1, 0, 0, 0, 0, 0, 0 },
-        { 0, 1, 0, 0, 0, 0, 0, 1, 0 },
-        { 1, 0, 1, 0, 0, 0, 1, 0, 0 },
-        { 1, 0, 0, 1, 0, 0, 0, 0, 0 }
+        { false, false, true,  true,  false, false, false, true,  true  },
+        { false, false, false, false, false, true,  true,  false, false },
+        { true,  false, false, false, true,  true,  false, true,  false },
+        { true,  false, false, false, false, false, false, false, true  },
+        { false, false, true,  false, false, false, false, false, false },
+        { false, true,  true,  false, false, false, false, false, false },
+        { false, true,  false, false, false, false, false, true,  false },
+        { true,  false, true,  false, false, false, true,  false, false },
+        { true,  false, false, true,  false, false, false, false, false }
     };
 
     bool* pMap = reinterpret_cast<bool*>(map);
+    unsigned int start;
 
-    unsigned int start = 2;
+    do {
 
-    Graph::searchDfs(start, pMap, GRAPH_SIZE, std::cout);
-    std::cout << std::endl;
-    Graph::searchBfs(start, pMap, GRAPH_SIZE, std::cout);
+        std::cout << "Enter start point: ";
+        std::cin >> start;
 
-//    do {
-//
-//        std::cout << "Enter start point: ";
-//        std::cin >> start;
-//
-//        std::cout << "DFS: ";
-//        Graph::searchDfs(start, map, std::cout);
-//        std::cout << std::endl;
-//
-//        std::cout << "BFS: ";
-//        Graph::searchBfs(start, map, std::cout);
-//        std::cout << std::endl;
-//        std::cout << std::endl;
-//
-//    } while (start != 0);
+        std::cout << "DFS: ";
+        Graph::searchDfs(start, pMap, GRAPH_SIZE, std::cout);
+        std::cout << std::endl;
+
+        std::cout << "BFS: ";
+        Graph::searchBfs(start, pMap, GRAPH_SIZE, std::cout);
+        std::cout << std::endl;
+        std::cout << std::endl;
+
+    } while (start != 0);
 }
-
-
-//#include <iostream>
-//using namespace std;
-//int mas[7][7] = { { 0, 1, 1, 0, 0, 0, 1 }, // матрица смежности
-//                  { 1, 0, 1, 1, 0, 0, 0 },
-//                  { 1, 1, 0, 0, 0, 0, 0 },
-//                  { 0, 1, 0, 0, 1, 0, 0 },
-//                  { 0, 0, 0, 1, 0, 1, 0 },
-//                  { 0, 0, 0, 0, 1, 0, 1 },
-//                  { 1, 0, 0, 0, 0, 1, 0 } };
-//int nodes[7]; // вершины графа
-//void search(int st, int n)
-//{
-//    int r;
-//    cout << st + 1 << " ";
-//    nodes[st] = 1;
-//    for (r = 0; r < n; r++)
-//        if (nodes[r] == 0)
-//            search(r, n);
-//}
-//int main()
-//{
-//    for (int i = 0; i < 7; i++) // исходно все вершины равны 0
-//        nodes[i] = 0;
-//    search(0, 7);
-//    cin.get();
-//    return 0;
-//}
-#pragma clang diagnostic pop
