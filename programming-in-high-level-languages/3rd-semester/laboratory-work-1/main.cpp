0a646cc63d17476ff02e948a0e77834504a244af
Andrew Piksaev <pikcaev@gmail.com>
2020-09-19 18:17:24 +0300

    Added main.cpp

--- programming-in-high-level-languages/3rd-semester/laboratory-work-1/main.cpp
+++ programming-in-high-level-languages/3rd-semester/laboratory-work-1/main.cpp
@@ -4,52 +4,89 @@
 
 #include "Stack.hpp"
 
+struct TempStruct {
+	int value1 = 0;
+	int value2 = 0;
+
+	TempStruct(int v1, int v2)
+	{
+		this->value1 = v1;
+		this->value2 = v2;
+	}
+
+	friend std::ostream& operator<< (std::ostream& out, const TempStruct& tempStruct)
+	{
+		return out << "{ " << tempStruct.value1 << "; " << tempStruct.value2 << " }";
+	}
+};
+
 int main() {
-	lab::Stack<int> stack;
+	// стек со списком инициализации
+	lab::Stack<int> stackList ({ 1, 2, 3, 4, 5, 6, 7 });
+	std::cout << "stack init list: " << stackList << std::endl;
 
-	std::cout << "stack size:  " << stack.size() << std::endl;
-	std::cout << "stack empty: " << stack.empty() << std::endl << std::endl;
+	// стек по дефолту
+	lab::Stack<int> stackDefault;
+	stackDefault.push(1);
+	stackDefault.push(2);
+	stackDefault.push(3);
+	stackDefault.push(4);
+	stackDefault.push(5);
+	stackDefault.push(6);
+	stackDefault.push(7);
+	std::cout << "stack default:   " << stackDefault << std::endl;
 
-	stack.push(1);
-	std::cout << "stack size:  " << stack.size() << std::endl;
-	std::cout << "check pop:   " << stack.check_pop() << std::endl;
-	std::cout << "stack empty: " << stack.empty() << std::endl << std::endl;
+	// стек через копирование
+	lab::Stack<int> stackCopy(stackDefault);
+	std::cout << "stack copy:      " << stackCopy << std::endl;
 
-	stack.push(2);
-	stack.push(3);
-	stack.push(4);
-
-	lab::Stack<int> stack2 (stack);
-	std::cout << "stack2: " << stack2 << std::endl << std::endl;
-	stack2.clear();
 	std::cout << std::endl;
 
-	std::cout << "stack1: " << stack << std::endl << std::endl;
+	// изъятие элементов из стека
+	try {
+		auto elementsNum = stackDefault.size();
+		for (std::size_t i = 0; i < elementsNum; i++) {
+			std::cout << "popped element: " << stackDefault.pop() << std::endl;
+		}
 
-	int tmp;
+		// искусственно делаем ошибку, которая выбросит исключение:
+		// все элементы уже извлечены, но мы пытаемся получить очередное значение
+		std::cout << "check pop: " << stackDefault.check_pop() << std::endl << std::endl;
 
-	tmp = stack.pop();
-	std::cout << "popped " << tmp << std::endl;
+	} catch (lab::PopOutOfRange& e) {
+		std::cout << std::endl << "[ERROR] cannot pop element" << std::endl << std::endl;
+	}
 
-	tmp = stack.pop();
-	std::cout << "popped " << tmp << std::endl << std::endl;
+	// проверяем, пустой ли стек после извлечения элементов
+	std::cout << "stack default empty: " << stackDefault.empty() << std::endl;
 
-	std::cout << "check pop:  " << stack.check_pop() << std::endl;
-	std::cout << "stack size: " << stack.size() << std::endl << std::endl;
+	std::cout << std::endl;
 
-	tmp = stack.pop();
-	std::cout << "popped " << tmp << std::endl;
+	// снова выводим стеки
+	std::cout << "stack init list: " << stackList << std::endl;
+	std::cout << "stack default:   " << stackDefault << std::endl;
+	std::cout << "stack copy:      " << stackCopy << std::endl;
 
-	tmp = stack.pop();
-	std::cout << "popped " << tmp << std::endl << std::endl;
+	std::cout << std::endl;
 
-	std::cout << "stack size:  " << stack.size() << std::endl;
-	std::cout << "stack empty: " << stack.empty() << std::endl << std::endl;
+	// очищаем стек
+	stackList.clear();
+	std::cout << "stack init list empty (after clear): " << stackList.empty() << std::endl;
 
-	stack.push(34);
-	stack.push(35);
-	stack.push(37);
-	stack.clear();
+	std::cout << std::endl;
 
-	stack.push(40);
+	// проверка стека на других типах
+	try {
+		lab::Stack<double> stackDouble ({ 1.2, 3.4, 5.6, 7.8, 9.0 });
+		std::cout << "double: " << stackDouble << std::endl;
+
+		lab::Stack<TempStruct> stackStruct ({ TempStruct(1, 2), TempStruct(3, 4), TempStruct(5, 6) });
+		std::cout << "struct: " << stackStruct << std::endl;
+
+		lab::Stack<std::string> stackString ({ "1.2", "3.4", "5.6", "7.8", "9.0" });
+		std::cout << "string: " << stackString << std::endl;
+	} catch (std::exception& e) {
+		std::cout << "[ERROR] " << e.what() << std::endl;
+	}
+
 }
