341729539b59b9dc0de78b539a6333043b7c7f6a
Андрей Пиксаев <pikcaev@gmail.com>
2020-12-09 10:09:32 +0300

    Добавлена лаб№4 по Java

--- java/3rd-semester/laboratory-work-4/src/com/main/Main.java
+++ java/3rd-semester/laboratory-work-4/src/com/main/Main.java
@@ -8,49 +8,49 @@ public class Main {
 
     public static void main(String[] args) {
 
-    	// ----------- STACK ----------- //
-		Stack stack = new Stack();
+        // ----------- STACK ----------- //
+        Stack stack = new Stack();
 
-		stack.push(1);
-		stack.push(2);
-		stack.push(3);
-		stack.push(4);
-		stack.push(5);
-		System.out.println(stack);
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+        stack.push(4);
+        stack.push(5);
+        System.out.println(stack);
 
-		stack.pop();
-		stack.pop();
-		System.out.println(stack);
+        stack.pop();
+        stack.pop();
+        System.out.println(stack);
 
-		System.out.println();
+        System.out.println();
 
-		// ----------- QUEUE ----------- //
-		Queue queue = new Queue();
+        // ----------- QUEUE ----------- //
+        Queue queue = new Queue();
 
-		queue.push(1);
-		queue.push(2);
-		queue.push(3);
-		queue.push(4);
-		queue.push(5);
-		System.out.println(queue);
+        queue.push(1);
+        queue.push(2);
+        queue.push(3);
+        queue.push(4);
+        queue.push(5);
+        System.out.println(queue);
 
-		queue.pop();
-		queue.pop();
-		System.out.println(queue);
+        queue.pop();
+        queue.pop();
+        System.out.println(queue);
 
-		System.out.println();
+        System.out.println();
 
-		// ----------- DACK ----------- //
-		Dack dack = new Dack();
+        // ----------- DACK ----------- //
+        Dack dack = new Dack();
 
-		dack.pushStart(1);
-		dack.pushStart(2);
-		dack.pushEnd(3);
-		dack.pushEnd(4);
-		System.out.println(dack);
+        dack.pushStart(1);
+        dack.pushStart(2);
+        dack.pushEnd(3);
+        dack.pushEnd(4);
+        System.out.println(dack);
 
-		dack.popStart();
-		dack.popEnd();
-		System.out.println(dack);
+        dack.popStart();
+        dack.popEnd();
+        System.out.println(dack);
     }
 }
